36639a1fb7d910afc42da766011a135a8e57c1f9
Shlomo Elbaum <100076995+Shloimy15e@users.noreply.github.com>
2025-06-02 22:01:17 +0000

    Mastermind

---
+++ mastermind/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header class="flex gap-0 items-center justify-start fixed top-0 start-0 w-full p-4">
+    <img alt="Vue logo" class="w-16 aspect-square object-cover" src="@/assets/logo.png" /> 
+    <h1 class="font-semibold text-lg">Mastermind</h1>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
